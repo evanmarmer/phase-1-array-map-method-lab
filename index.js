7935793eac12c27e5b5e1f928f33f221b3ccb561
evanmarmer <evanmarmer@gmail.com>
2023-03-24 14:12:24 -0600

    map method lab

--- index.js
+++ index.js
@@ -11,6 +11,31 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
-}
+// const titleCased = () => {
+//   return tutorials
+// }
+
+// const titleCased = () => {
+//     return tutorials.map(tutorial => {
+//       const arr = tutorial.split(" ")
+//       for (var i = 0; i < arr.length; i++) {
+//         arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
+//       }
+//       const str2 = arr.join(" ")
+//       return str2
+//     }
+//     )
+//   }
+  
+  const titleCased = () => {
+    return tutorials.map(tutorial => {
+      const arr = tutorial.split(" ")
+      const capitilize = arr.map(
+        arr => arr.charAt(0).toUpperCase() + arr.slice(1)
+      )
+      const str2 = capitilize.join(" ")
+      return str2
+      })
+    }
+   
+  
